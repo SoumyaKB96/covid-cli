bf64381bd9d0f3cb7512d2e4ccf49e03b31c815e
SoumyakB96 <boity.soumyakanta@gmail.com>
2020-05-04 20:14:17 +0530

    Added international cases

--- actions.py
+++ actions.py
@@ -19,11 +19,26 @@ def state_case(key):
         if(key==i["state"]):
             for x in i:
                 print(x,"\t", i[x])
+            break    
               
     
-        
-    
-    
+data_int = requests.get("https://api.covid19api.com/summary")
 
+
+    
+def case2():
+ dat = data_int.json()["Global"]
+ for i in dat:
+    print(i,"\t", dat[i])
+
+  
+def cont_case(key):
+    dat = data_int.json()["Countries"]
+
+    for i in dat:
+        if(key == i["Country"]):
+            for x in i:
+                print(x, "\t", i[x])
+            break    
 
 
--- app.py
+++ app.py
@@ -2,14 +2,15 @@ import sys
 import actions
 
 
-
-
-
 if(sys.argv[1]=="-t"):
     print(actions.cases())
     
 if(sys.argv[1]=="-s"):
     print(actions.state_case(sys.argv[-1]))
 
-
+if(sys.argv[1]=="-i"):
+    if(sys.argv[-1]=="-i"):
+     print(actions.case2())
+    else :
+        print(actions.cont_case(sys.argv[-1])) 
 
